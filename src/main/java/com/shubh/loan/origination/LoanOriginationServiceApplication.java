c5bd4c7e91a331b3feb86058b747b5f67a823667
ShubhMehrotra <ShubhMehro@gmail.com>
2026-03-03 20:16:11 +0530

    Inital setup of the project

---
+++ src/main/java/com/shubh/loan/origination/LoanOriginationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shubh.loan.origination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanOriginationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanOriginationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubh/loan/origination/LoanOriginationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubh.loan.origination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanOriginationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
