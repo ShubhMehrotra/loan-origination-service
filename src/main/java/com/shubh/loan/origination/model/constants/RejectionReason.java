d07fa5ec5ecb7148e0f2eca8e985430623c1a359
ShubhMehrotra <ShubhMehro@gmail.com>
2026-03-21 13:21:03 +0530

    Added constants enums for rejection and risk band

--- src/main/java/com/shubh/loan/origination/model/constants/RejectionReason.java
+++ src/main/java/com/shubh/loan/origination/model/constants/RejectionReason.java
@@ -1,4 +1,8 @@
 package com.shubh.loan.origination.model.constants;
 
 public enum RejectionReason {
+    LOW_CREDIT_SCORE,
+    AGE_TENURE_LIMIT_EXCEEDED,
+    EMI_EXCEEDS_60_PERCENT,
+    EMI_EXCEEDS_50_PERCENT
 }
--- src/main/java/com/shubh/loan/origination/model/constants/RiskBand.java
+++ src/main/java/com/shubh/loan/origination/model/constants/RiskBand.java
@@ -1,4 +1,7 @@
 package com.shubh.loan.origination.model.constants;
 
 public enum RiskBand {
+    LOW,
+    MEDIUM,
+    HIGH
 }
